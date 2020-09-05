f2481f69aeff906cfd09858ed390772848bccd32
James Yuzawa <jtyuzawa@gmail.com>
2020-09-04 23:18:30 -0400

    use interfaces

--- src/main/java/com/jyuzawa/googolplex_theater/GoogolplexTheater.java
+++ src/main/java/com/jyuzawa/googolplex_theater/GoogolplexTheater.java
@@ -1,6 +1,7 @@
 package com.jyuzawa.googolplex_theater;
 
 import com.jyuzawa.googolplex_theater.client.GoogolplexController;
+import com.jyuzawa.googolplex_theater.client.GoogolplexControllerImpl;
 import com.jyuzawa.googolplex_theater.config.CastConfigLoader;
 import com.jyuzawa.googolplex_theater.config.Config;
 import com.jyuzawa.googolplex_theater.mdns.ServiceDiscovery;
@@ -20,45 +21,41 @@ import org.slf4j.LoggerFactory;
  * @author jyuzawa
  */
 public final class GoogolplexTheater {
-  private static final Logger LOG = LoggerFactory.getLogger(GoogolplexTheater.class);
-  public static final String PROJECT_WEBSITE = "https://github.com/yuzawa-san/googolplex-theater";
+	private static final Logger LOG = LoggerFactory.getLogger(GoogolplexTheater.class);
+	public static final String PROJECT_WEBSITE = "https://github.com/yuzawa-san/googolplex-theater";
 
-  public static void main(String[] args) throws InterruptedException {
-    try {
-      Config config = new Config(args);
-      LOG.info("Starting up Googolplex Theater!");
-      LOG.info("Website: " + PROJECT_WEBSITE);
-      Vertx vertx = Vertx.vertx();
-      GoogolplexController controller =
-          new GoogolplexController(vertx.nettyEventLoopGroup(), config.getAppId());
-      vertx.deployVerticle(new GoogolplexServer(controller, config.getServerPort()));
-      CastConfigLoader configLoader = new CastConfigLoader(controller, config.getCastConfigPath());
-      ServiceDiscovery serviceDiscovery =
-          new ServiceDiscovery(controller, config.getInterfaceAddress());
-      // collect items to close on shutdown
-      List<Closeable> tasks = Arrays.asList(configLoader, serviceDiscovery);
-      Runtime.getRuntime()
-          .addShutdownHook(
-              new Thread(
-                  () -> {
-                    LOG.info("Shutting down Googolplex Theater!");
-                    for (Closeable task : tasks) {
-                      try {
-                        task.close();
-                      } catch (Exception e) {
-                        LOG.warn("Failed to shut down", e);
-                      }
-                    }
-                    vertx.close();
-                  }));
-    } catch (ParseException e) {
-      System.out.println(e.getClass().getSimpleName() + ": " + e.getMessage());
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp("googolplex-theater", Config.OPTIONS, true);
-      System.exit(1);
-    } catch (Exception e) {
-      LOG.error("Failed to start", e);
-      System.exit(1);
-    }
-  }
+	public static void main(String[] args) throws InterruptedException {
+		try {
+			Config config = new Config(args);
+			LOG.info("Starting up Googolplex Theater!");
+			LOG.info("Website: " + PROJECT_WEBSITE);
+			Vertx vertx = Vertx.vertx();
+			GoogolplexController controller = new GoogolplexControllerImpl(vertx.nettyEventLoopGroup(),
+					config.getAppId());
+			vertx.deployVerticle(new GoogolplexServer(controller, config.getServerPort()));
+			CastConfigLoader configLoader = new CastConfigLoader(controller, config.getCastConfigPath());
+			ServiceDiscovery serviceDiscovery = new ServiceDiscovery(controller, config.getInterfaceAddress());
+			// collect items to close on shutdown
+			List<Closeable> tasks = Arrays.asList(configLoader, serviceDiscovery);
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				LOG.info("Shutting down Googolplex Theater!");
+				for (Closeable task : tasks) {
+					try {
+						task.close();
+					} catch (Exception e) {
+						LOG.warn("Failed to shut down", e);
+					}
+				}
+				vertx.close();
+			}));
+		} catch (ParseException e) {
+			System.out.println(e.getClass().getSimpleName() + ": " + e.getMessage());
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("googolplex-theater", Config.OPTIONS, true);
+			System.exit(1);
+		} catch (Exception e) {
+			LOG.error("Failed to start", e);
+			System.exit(1);
+		}
+	}
 }
--- src/test/java/com/jyuzawa/googolplex_theater/client/GoogolplexControllerTest.java
+++ src/test/java/com/jyuzawa/googolplex_theater/client/GoogolplexControllerTest.java
@@ -25,168 +25,158 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class GoogolplexControllerTest {
-  private static final Logger LOG = LoggerFactory.getLogger(GoogolplexControllerTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(GoogolplexControllerTest.class);
 
-  static GoogolplexController controller;
-  static FakeCast cast1;
-  static FakeCast cast2;
-  static FakeCast cast3;
-  static FakeCast cast4;
-  static EventLoopGroup workerGroup;
+	static GoogolplexController controller;
+	static FakeCast cast1;
+	static FakeCast cast2;
+	static FakeCast cast3;
+	static FakeCast cast4;
+	static EventLoopGroup workerGroup;
 
-  @BeforeAll
-  static void setUpBeforeClass() throws Exception {
-    workerGroup = new NioEventLoopGroup(1);
-    controller =
-        new GoogolplexController(workerGroup, GoogolplexClientHandler.DEFAULT_APPLICATION_ID, 0, 0);
-    cast1 = new FakeCast(workerGroup, 9001);
-    cast2 = new FakeCast(workerGroup, 9002);
-    cast3 = new FakeCast(workerGroup, 9003);
-    cast4 = new FakeCast(workerGroup, 9004);
-  }
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		workerGroup = new NioEventLoopGroup(1);
+		controller = new GoogolplexControllerImpl(workerGroup, GoogolplexClientHandler.DEFAULT_APPLICATION_ID, 0, 0);
+		cast1 = new FakeCast(workerGroup, 9001);
+		cast2 = new FakeCast(workerGroup, 9002);
+		cast3 = new FakeCast(workerGroup, 9003);
+		cast4 = new FakeCast(workerGroup, 9004);
+	}
 
-  @AfterAll
-  static void tearDownAfterClass() throws Exception {
-    CastConfig newConfig = new CastConfig(Collections.emptyList());
-    controller.accept(newConfig);
-    cast1.close();
-    cast2.close();
-    cast3.close();
-    cast4.close();
-    workerGroup.shutdownGracefully().syncUninterruptibly();
-  }
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		CastConfig newConfig = new CastConfig(Collections.emptyList());
+		controller.accept(newConfig);
+		cast1.close();
+		cast2.close();
+		cast3.close();
+		cast4.close();
+		workerGroup.shutdownGracefully().syncUninterruptibly();
+	}
 
-  @Test
-  void test() throws Exception {
-    List<DeviceInfo> devices = new ArrayList<>();
-    devices.add(cast1.device());
-    devices.add(cast2.device());
-    devices.add(cast3.device());
-    devices.add(cast4.device());
-    CastConfig config = new CastConfig(devices);
-    controller.register(cast1.event());
-    controller.register(cast2.event());
-    controller.accept(config);
-    controller.register(cast3.event());
-    controller.register(cast4.event());
-    controller.register(FakeCast.event(9005, "UnknownCast"));
-    ServiceEvent noName = Mockito.mock(ServiceEvent.class);
-    ServiceInfo noNameInfo = Mockito.mock(ServiceInfo.class);
-    Mockito.when(noName.getInfo()).thenReturn(noNameInfo);
-    Mockito.when(noNameInfo.getPropertyString(Mockito.anyString())).thenReturn(null);
-    controller.register(noName);
-    ServiceEvent noAddr = Mockito.mock(ServiceEvent.class);
-    Mockito.when(noAddr.getName()).thenReturn("Chromecast-NOIP.local");
-    ServiceInfo noAddrInfo = Mockito.mock(ServiceInfo.class);
-    Mockito.when(noAddr.getInfo()).thenReturn(noAddrInfo);
-    Mockito.when(noAddrInfo.getPropertyString(Mockito.anyString())).thenReturn("NOIP");
-    Mockito.when(noAddrInfo.getInetAddresses()).thenReturn(new InetAddress[] {});
-    controller.register(noAddr);
+	@Test
+	void test() throws Exception {
+		List<DeviceInfo> devices = new ArrayList<>();
+		devices.add(cast1.device());
+		devices.add(cast2.device());
+		devices.add(cast3.device());
+		devices.add(cast4.device());
+		CastConfig config = new CastConfig(devices);
+		controller.register(cast1.event());
+		controller.register(cast2.event());
+		controller.accept(config);
+		controller.register(cast3.event());
+		controller.register(cast4.event());
+		controller.register(FakeCast.event(9005, "UnknownCast"));
+		ServiceEvent noName = Mockito.mock(ServiceEvent.class);
+		ServiceInfo noNameInfo = Mockito.mock(ServiceInfo.class);
+		Mockito.when(noName.getInfo()).thenReturn(noNameInfo);
+		Mockito.when(noNameInfo.getPropertyString(Mockito.anyString())).thenReturn(null);
+		controller.register(noName);
+		ServiceEvent noAddr = Mockito.mock(ServiceEvent.class);
+		Mockito.when(noAddr.getName()).thenReturn("Chromecast-NOIP.local");
+		ServiceInfo noAddrInfo = Mockito.mock(ServiceInfo.class);
+		Mockito.when(noAddr.getInfo()).thenReturn(noAddrInfo);
+		Mockito.when(noAddrInfo.getPropertyString(Mockito.anyString())).thenReturn("NOIP");
+		Mockito.when(noAddrInfo.getInetAddresses()).thenReturn(new InetAddress[] {});
+		controller.register(noAddr);
 
-    List<JsonObject> unconfigureds = controller.getUnconfiguredDevices();
-    assertEquals(1, unconfigureds.size());
-    JsonObject unconfigured = unconfigureds.get(0);
-    assertEquals("UnknownCast", unconfigured.getString("name"));
+		List<JsonObject> unconfigureds = controller.getUnconfiguredDevices();
+		assertEquals(1, unconfigureds.size());
+		JsonObject unconfigured = unconfigureds.get(0);
+		assertEquals("UnknownCast", unconfigured.getString("name"));
 
-    List<JsonObject> configureds = controller.getConfiguredDevices();
-    assertEquals(4, configureds.size());
-    assertEquals(cast1.name, configureds.get(0).getString("name"));
-    assertEquals(cast2.name, configureds.get(1).getString("name"));
-    assertEquals(cast3.name, configureds.get(2).getString("name"));
-    assertEquals(cast4.name, configureds.get(3).getString("name"));
+		List<JsonObject> configureds = controller.getConfiguredDevices();
+		assertEquals(4, configureds.size());
+		assertEquals(cast1.name, configureds.get(0).getString("name"));
+		assertEquals(cast2.name, configureds.get(1).getString("name"));
+		assertEquals(cast3.name, configureds.get(2).getString("name"));
+		assertEquals(cast4.name, configureds.get(3).getString("name"));
 
-    assertTransaction(cast1);
-    assertTransaction(cast2);
-    assertTransaction(cast3);
-    assertTransaction(cast4);
+		assertTransaction(cast1);
+		assertTransaction(cast2);
+		assertTransaction(cast3);
+		assertTransaction(cast4);
 
-    // idle screen on 2
-    cast2.loadIdleScreen();
-    assertTransaction(cast2);
+		// idle screen on 2
+		cast2.loadIdleScreen();
+		assertTransaction(cast2);
 
-    // force close on 3
-    cast3.closeChannel();
-    assertTransaction(cast3);
+		// force close on 3
+		cast3.closeChannel();
+		assertTransaction(cast3);
 
-    // update 1, delete 4
-    cast1.custom = "new";
-    devices.set(0, cast1.device());
-    devices.remove(3);
-    CastConfig newConfig = new CastConfig(devices);
-    controller.accept(newConfig);
-    assertTransaction(cast1);
+		// update 1, delete 4
+		cast1.custom = "new";
+		devices.set(0, cast1.device());
+		devices.remove(3);
+		CastConfig newConfig = new CastConfig(devices);
+		controller.accept(newConfig);
+		assertTransaction(cast1);
 
-    List<JsonObject> unconfigureds2 = controller.getUnconfiguredDevices();
-    assertEquals(2, unconfigureds2.size());
-    List<String> unconfigureds2Names = new ArrayList<>();
-    for (JsonObject device : unconfigureds2) {
-      unconfigureds2Names.add(device.getString("name"));
-    }
-    assertTrue(unconfigureds2Names.contains(cast4.name));
-    assertTrue(unconfigureds2Names.contains("UnknownCast"));
+		List<JsonObject> unconfigureds2 = controller.getUnconfiguredDevices();
+		assertEquals(2, unconfigureds2.size());
+		List<String> unconfigureds2Names = new ArrayList<>();
+		for (JsonObject device : unconfigureds2) {
+			unconfigureds2Names.add(device.getString("name"));
+		}
+		assertTrue(unconfigureds2Names.contains(cast4.name));
+		assertTrue(unconfigureds2Names.contains("UnknownCast"));
 
-    List<JsonObject> configureds2 = controller.getConfiguredDevices();
-    assertEquals(3, configureds2.size());
-    List<String> configureds2Name = new ArrayList<>();
-    for (JsonObject device : configureds2) {
-      configureds2Name.add(device.getString("name"));
-    }
-    assertTrue(configureds2Name.contains(cast1.name));
-    assertTrue(configureds2Name.contains(cast2.name));
-    assertTrue(configureds2Name.contains(cast3.name));
+		List<JsonObject> configureds2 = controller.getConfiguredDevices();
+		assertEquals(3, configureds2.size());
+		List<String> configureds2Name = new ArrayList<>();
+		for (JsonObject device : configureds2) {
+			configureds2Name.add(device.getString("name"));
+		}
+		assertTrue(configureds2Name.contains(cast1.name));
+		assertTrue(configureds2Name.contains(cast2.name));
+		assertTrue(configureds2Name.contains(cast3.name));
 
-    // refresh 2
-    controller.refresh(cast2.name);
-    assertTransaction(cast2);
+		// refresh 2
+		controller.refresh(cast2.name);
+		assertTransaction(cast2);
 
-    // refresh all
-    controller.refresh(null);
-    assertTransaction(cast1);
-    assertTransaction(cast2);
-    assertTransaction(cast3);
+		// refresh all
+		controller.refresh(null);
+		assertTransaction(cast1);
+		assertTransaction(cast2);
+		assertTransaction(cast3);
 
-    // send broken messages which should restart 3
-    cast3.sendBrokenMessages();
-    assertTransaction(cast3);
+		// send broken messages which should restart 3
+		cast3.sendBrokenMessages();
+		assertTransaction(cast3);
 
-    LOG.info("DONE");
-  }
+		LOG.info("DONE");
+	}
 
-  private void assertTransaction(FakeCast cast) throws Exception {
-    CastMessage connect = cast.getMessage();
-    assertType(
-        connect,
-        GoogolplexClientHandler.DEFAULT_RECEIVER_ID,
-        GoogolplexClientHandler.NAMESPACE_CONNECTION);
-    assertEquals("{\"type\":\"CONNECT\"}", connect.getPayloadUtf8());
+	private void assertTransaction(FakeCast cast) throws Exception {
+		CastMessage connect = cast.getMessage();
+		assertType(connect, GoogolplexClientHandler.DEFAULT_RECEIVER_ID, GoogolplexClientHandler.NAMESPACE_CONNECTION);
+		assertEquals("{\"type\":\"CONNECT\"}", connect.getPayloadUtf8());
 
-    CastMessage launch = cast.getMessage();
-    assertType(
-        launch,
-        GoogolplexClientHandler.DEFAULT_RECEIVER_ID,
-        GoogolplexClientHandler.NAMESPACE_RECEIVER);
-    assertEquals(
-        "{\"requestId\":0,\"appId\":\""
-            + GoogolplexClientHandler.DEFAULT_APPLICATION_ID
-            + "\",\"type\":\"LAUNCH\"}",
-        launch.getPayloadUtf8());
+		CastMessage launch = cast.getMessage();
+		assertType(launch, GoogolplexClientHandler.DEFAULT_RECEIVER_ID, GoogolplexClientHandler.NAMESPACE_RECEIVER);
+		assertEquals("{\"requestId\":0,\"appId\":\"" + GoogolplexClientHandler.DEFAULT_APPLICATION_ID
+				+ "\",\"type\":\"LAUNCH\"}", launch.getPayloadUtf8());
 
-    CastMessage appConnect = cast.getMessage();
-    assertType(appConnect, cast.toString(), GoogolplexClientHandler.NAMESPACE_CONNECTION);
-    assertEquals("{\"type\":\"CONNECT\"}", appConnect.getPayloadUtf8());
+		CastMessage appConnect = cast.getMessage();
+		assertType(appConnect, cast.toString(), GoogolplexClientHandler.NAMESPACE_CONNECTION);
+		assertEquals("{\"type\":\"CONNECT\"}", appConnect.getPayloadUtf8());
 
-    CastMessage app = cast.getMessage();
-    assertType(app, cast.toString(), GoogolplexClientHandler.NAMESPACE_CUSTOM);
-    JsonNode node = JsonUtil.MAPPER.readTree(app.getPayloadUtf8());
-    assertEquals(cast.name, node.get("name").asText());
-    assertEquals(cast.custom, node.get("settings").get("foo").asText());
-  }
+		CastMessage app = cast.getMessage();
+		assertType(app, cast.toString(), GoogolplexClientHandler.NAMESPACE_CUSTOM);
+		JsonNode node = JsonUtil.MAPPER.readTree(app.getPayloadUtf8());
+		assertEquals(cast.name, node.get("name").asText());
+		assertEquals(cast.custom, node.get("settings").get("foo").asText());
+	}
 
-  private void assertType(CastMessage msg, String receiverId, String namespace) {
-    assertEquals(CastMessage.ProtocolVersion.CASTV2_1_0, msg.getProtocolVersion());
-    assertTrue(msg.getSourceId().startsWith("sender-"));
-    assertEquals(receiverId, msg.getDestinationId());
-    assertEquals(namespace, msg.getNamespace());
-    assertEquals(CastMessage.PayloadType.STRING, msg.getPayloadType());
-  }
+	private void assertType(CastMessage msg, String receiverId, String namespace) {
+		assertEquals(CastMessage.ProtocolVersion.CASTV2_1_0, msg.getProtocolVersion());
+		assertTrue(msg.getSourceId().startsWith("sender-"));
+		assertEquals(receiverId, msg.getDestinationId());
+		assertEquals(namespace, msg.getNamespace());
+		assertEquals(CastMessage.PayloadType.STRING, msg.getPayloadType());
+	}
 }
