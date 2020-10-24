fc7ee82c5708dcd3fa45980fd679daf7ba4129ae
James Yuzawa <jtyuzawa@gmail.com>
2020-10-24 11:55:10 -0400

    add diagnostic information

--- src/main/java/com/jyuzawa/googolplex_theater/GoogolplexTheater.java
+++ src/main/java/com/jyuzawa/googolplex_theater/GoogolplexTheater.java
@@ -7,6 +7,9 @@ import com.jyuzawa.googolplex_theater.config.Config;
 import com.jyuzawa.googolplex_theater.mdns.ServiceDiscovery;
 import com.jyuzawa.googolplex_theater.server.GoogolplexServer;
 import io.vertx.core.Vertx;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
@@ -20,12 +23,23 @@ import org.slf4j.LoggerFactory;
 public final class GoogolplexTheater {
   private static final Logger LOG = LoggerFactory.getLogger(GoogolplexTheater.class);
   public static final String PROJECT_WEBSITE = "https://github.com/yuzawa-san/googolplex-theater";
+  private static final List<String> DIAGNOSTIC_PROPERTIES =
+      Collections.unmodifiableList(
+          Arrays.asList("os.name", "os.version", "os.arch", "java.vendor", "java.version"));
 
   public static void main(String[] args) {
     try {
       Config config = new Config(args);
       LOG.info("Starting up Googolplex Theater!");
       LOG.info("Website: " + PROJECT_WEBSITE);
+      Package thePackage = GoogolplexTheater.class.getPackage();
+      LOG.info(
+          "Version: {} ({})",
+          thePackage.getSpecificationVersion(),
+          thePackage.getImplementationVersion());
+      for (String property : DIAGNOSTIC_PROPERTIES) {
+        LOG.info("Runtime[{}]: {}", property, System.getProperty(property));
+      }
       Vertx vertx = Vertx.vertx();
       GoogolplexController controller =
           new GoogolplexControllerImpl(vertx.nettyEventLoopGroup(), config.getAppId());
