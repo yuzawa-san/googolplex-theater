522ab1dd418c6d816bd2b25a30fc7ab0ba943c5e
James Yuzawa <jtyuzawa@gmail.com>
2021-11-09 09:22:17 -0500

    wip

---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,28 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
+
+import com.jyuzawa.googolplex_theater.GoogolplexTheater;
+
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = GoogolplexTheater.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
---
+++ src/test/resources/hellocucumber/is_it_friday_yet.feature
@@ -0,0 +1,14 @@
+Feature: Is it Friday yet?
+  Everybody wants to know when it's Friday
+
+  # commment does here
+  Scenario Outline: Today is or is not Friday
+    Given today is "<day>"
+    When I ask whether it's Friday yet
+    Then I should be told "<answer>"
+
+  Examples:
+    | day            | answer |
+    | Friday         | TGIF   |
+    | Sunday         | Nope   |
+    | anything else! | Nope   |
